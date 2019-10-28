3c7f820805c63e4c7ff090816f32130537a9ef33
tastytea <mrarttermos@gmail.com>
2019-10-28 16:37:04 +0600

    Prefix added, validations merged to one string

--- app/models/fixed_cost.rb
+++ app/models/fixed_cost.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class FixedCost < ApplicationRecord
-  validates :name, presence: true
-  validates :cost, presence: true
-  enum frequency: { once: 0, daily: 1, weekly: 2, monthly: 3, yearly: 4 }
+  validates :name, :cost, presence: true
+  enum frequency: { once: 0, daily: 1, weekly: 2, monthly: 3, yearly: 4 }, _prefix: :paid
 end
